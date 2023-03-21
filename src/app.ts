b21c75d4d7a0392b9c892fae6bac342848c0802b
ioetbc <ioetbc@gmail.com>
2023-03-21 12:19:36 +0000

    initial state

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import {App} from "@slack/bolt";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+// Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+// you still need to listen on some port!
+  port: 3000,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }: {message: any, say: any}) => {
+    console.log('message', message)
+  await say(`Hey there <@${message.user}>!`);
+});
+
+app.command('/sql-dev', async ({ command, ack, respond }) => {
+  console.log('hello command', command);
+  await ack();
+  await respond(`${command.text}`);
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log("⚡️ Bolt app is running!");
+})();
